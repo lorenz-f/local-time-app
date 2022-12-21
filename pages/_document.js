0e5d5c874f966c0b3d2e8858fe5c5d0730e79dda
lorenzof97 <67831959+lorenzof97@users.noreply.github.com>
2022-12-21 18:47:03 -0500

    major responsiveness and feature changes

--- pages/_document.js
+++ pages/_document.js
@@ -5,17 +5,37 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> {/* watch if setting cross origin affects anything */}
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />{" "}
+        {/* watch if setting cross origin affects anything */}
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
           rel="stylesheet"
         />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
-      <body className = "bg-[url(/horizon.jpg)] h-screen w-screen bg-cover bg-no-repeat font-montserrat overflow-hidden m-0 transition-all duration-300 ease-in">
+      <body
+        id={`sm:bodystyle`}        
+        className="bg-[url(/horizon.jpg)] sm:bg-gradient-to-t sm:from-[#312E81] sm:to-[#FFB6C1] sm:overflow-x-hidden sm:overflow-y-hidden h-screen w-screen bg-cover bg-no-repeat font-montserrat overflow-hidden m-0 transition-all duration-300 ease-in"
+      >
         <Main />
         <NextScript />
       </body>
     </Html>
   );
 }
+
+/* 
+
+<body className = "bg-[url(/horizon.jpg)] h-[100vh] w-[100vw] bg-cover bg-no-repeat font-montserrat overflow-hidden m-0 transition-all duration-300 ease-in">
+        <Main />
+        <NextScript />
+      </body>
+
+*/
